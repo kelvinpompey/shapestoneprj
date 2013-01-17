7b694e40e86aa93d1a14235aecbf544863bd3072
Kelvin Pompey <kelvin.pompey@gmail.com>
2013-01-17 18:54:54 +0000

    added beginnings of server project model

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , taskboard = require('./routes/taskboard')
+  , mongoose = require('mongoose')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3030);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -32,6 +33,10 @@ app.get('/', routes.index);
 app.get('/taskboard', taskboard.index);
 app.get('/users', user.list);
 
+var db = mongoose.connect('mongodb://localhost/shapestonedb').connection;
+db.on('open', function(){
+    console.log('connection open');
+}); 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
